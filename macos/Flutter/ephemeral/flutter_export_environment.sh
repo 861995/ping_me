b54897180e975127edbefe40f5eb971649fbffa8
861995 <vk18rp08@gmail.com>
2024-09-18 22:42:10 +0530

    initial commit

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ramprakash/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ramprakash/AndroidStudioProjects/we_chat"
+export "FLUTTER_APPLICATION_PATH=/Users/ramprakash/AndroidStudioProjects/ping_me"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
